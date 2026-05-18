150a3a7fc564cb4ab85a5baf63f5e49089a4f042
sxtrng <sxtrng@gmail.com>
2026-05-18 09:04:11 -0400

    second program

---
+++ Easy_1B.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Easy_1B {
+    public static void main(String[] args) {
+
+        System.out.print("Glad you" + " ");
+        System.out.println("came back.");
+
+        System.exit(0);
+
+    }
+}
