aa4c939619f7893de8bdd8019049ae789a462c19
sxtrng <sxtrng@gmail.com>
2026-05-18 09:00:06 -0400

    program one

---
+++ Easy_1A.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Easy_1A {
+    public static void main(String[] args) {
+
+        System.out.println("This is our first program.");
+
+        System.exit(0);
+
+    }
+}
